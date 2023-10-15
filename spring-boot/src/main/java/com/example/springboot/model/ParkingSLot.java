35414db1344dfa1be54df9fa9a08dee80dbc0188
Ha <dieuhanguyenngoc@gmail.com>
2023-10-15 08:48:26 +0700

    add change file

--- spring-boot/src/main/java/com/example/springboot/model/ParkingSLot.java
+++ spring-boot/src/main/java/com/example/springboot/model/ParkingSLot.java
@@ -1,5 +1,10 @@
 package com.example.springboot.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 public class ParkingSLot {
+    private Long spotId;
     
 }
