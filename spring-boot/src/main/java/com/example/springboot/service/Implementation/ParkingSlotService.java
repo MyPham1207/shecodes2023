7c533d8397900f643d3f0665a2f34097a401c722
Ha <dieuhanguyenngoc@gmail.com>
2023-10-15 08:21:05 +0700

    add module file

---
+++ spring-boot/src/main/java/com/example/springboot/service/Implementation/ParkingSlotService.java
@@ -0,0 +1,5 @@
+package com.example.springboot.service.Implementation;
+
+public class ParkingSlotService {
+    
+}
---
+++ spring-boot/src/main/java/com/example/springboot/service/Interface/ParkingSlotInterface.java
@@ -0,0 +1,5 @@
+package com.example.springboot.service.Interface;
+
+public class ParkingSlotInterface {
+    
+}
